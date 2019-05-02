bc22ff13f5bb967f9a0e44456a02dff35be84cbf
nikolayemrikh <nikolayemrikh@htmlacademy.ru>
2019-05-03 01:51:33 +0300

    Правит и валидирует пропсы, добавляет упущенную верстку

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {Fragment} from "react";
+import Main from "./main/main.jsx";
 
 const App = () => {
+  const places = [{
+    name: `Beautiful & luxurious apartment at great location`,
+  }, {
+    name: `Wood and stone place`
+  }, {
+    name: `Canal View Prinsengracht`
+  }, {
+    name: `Nice, cozy, warm big bed apartment`
+  }, {
+    name: `Wood and stone place`
+  }];
   return (
-    <div></div>
+    <Fragment>
+      <Main places={places}/>
+    </Fragment>
   );
 };
 
